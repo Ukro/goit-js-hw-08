89acdeac5475207333e8c4fdd3306b38678c9315
Ukro <koliaso@gmail.com>
2023-08-27 17:03:42 +0300

    to-mentor

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,9 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const onPlay = time =>
+  localStorage.setItem('videoplayer-current-time', time.seconds);
+const currentTimeOfVideo = localStorage.getItem('videoplayer-current-time');
+currentTimeOfVideo ? player.setCurrentTime(currentTimeOfVideo) : null;
+player.on('timeupdate', throttle(onPlay, 1000));
